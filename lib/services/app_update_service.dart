4cec5f37523893460762a5a4ea244685d1f97f65
Vaibhav Jalota <vaibhavjalota06@gmail.com>
2026-08-15 16:24:45 +0530

    chore: Bump app version to 1.0.46 in pubspec.yaml and AppUpdateService, sync cross-platform releases

--- lib/services/app_update_service.dart
+++ lib/services/app_update_service.dart
@@ -23,8 +23,8 @@ class AppUpdateInfo {
 }
 
 class AppUpdateService {
-  static const String currentAppVersion = "1.0.0";
-  static const int currentBuildNumber = 1;
+  static const String currentAppVersion = "1.0.45";
+  static const int currentBuildNumber = 45;
 
   static final AppUpdateService _instance = AppUpdateService._internal();
   factory AppUpdateService() => _instance;
