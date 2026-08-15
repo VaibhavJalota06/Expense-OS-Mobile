7ed9ecfb3f43da297ed712a2095bdd3783a0ddf2
Vaibhav Jalota <vaibhavjalota06@gmail.com>
2026-08-15 13:49:25 +0530

    Fix imports for url_launcher and services in SplitBillScreen

--- lib/screens/split_bill_screen.dart
+++ lib/screens/split_bill_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../models/expense_model.dart';
 import '../services/currency_service.dart';
 import '../services/supabase_service.dart';
