62506201e6c3c38896d06f56f0941de1ff9615bd
Vaibhav Jalota <vaibhavjalota06@gmail.com>
2026-08-15 14:20:38 +0530

    Add missing SharedPreferences import for budget requirement enforcement

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/expense_model.dart';
 import '../services/currency_service.dart';
 import '../theme/app_theme.dart';
