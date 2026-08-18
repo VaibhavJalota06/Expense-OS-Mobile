3a8a245ac2c780e6fa46c7cd81da9d6e7e7317c6
Vaibhav Jalota <vaibhavjalota06@gmail.com>
2026-08-18 12:38:26 +0530

    ci: streamline build_release workflow for automated APK and IPA generation

--- lib/services/app_update_service.dart
+++ lib/services/app_update_service.dart
@@ -25,8 +25,8 @@ class AppUpdateInfo {
 }
 
 class AppUpdateService {
-  static const String currentAppVersion = "3.0.1";
-  static const int currentBuildNumber = 52;
+  static const String currentAppVersion = "3.0.2";
+  static const int currentBuildNumber = 53;
 
   static final AppUpdateService _instance = AppUpdateService._internal();
   factory AppUpdateService() => _instance;
